1d76f7436436d89a284c05855e7cf0cdca52ed28
Sam <sam_mcdo@yahoo.com>
2020-10-27 21:14:28 -0700

    feat: wrappers have lazy mode

---
+++ src/main/java/frc/robot/wrappers/MotorControllers/NomadSparkMaxBrushed.java
@@ -0,0 +1,45 @@
+package frc.robot.wrappers.MotorControllers;
+
+/**
+ * This class is an encapsulation of WPI_SparkMAX that add a couple constructors
+ * for forcing common settings.
+ */
+public class NomadSparkMaxBrushed extends NomadSparkMax {
+
+    /**
+     * Constructs a SparkMAX, reverts it to factory default, and sets brake mode.
+     * 
+     * @param port The CAN ID of this SparkMAX
+     */
+    public NomadSparkMaxBrushed(int port) {
+        super(port);
+        setMotorType(MotorType.kBrushed);
+        restoreFactoryDefaults();
+        setIdleMode(IdleMode.kBrake);
+    }
+
+    /**
+     * Constructs a SparkMAX, reverts it to factory default, and sets brake mode and
+     * inversion status.
+     * 
+     * @param port     The CAN ID of this SparkMAX.
+     * @param inverted True for inverted, false if not.
+     */
+    public NomadSparkMaxBrushed(int port, boolean inverted) {
+        this(port);
+        setInverted(inverted);
+    }
+
+    /**
+     * Constructs a SparkMAX, reverts it to factory default, sets brake mode and
+     * inversion status, and slaves it to a specified NomadSparkMAX.
+     * 
+     * @param port     The CAN ID of this SparkMAX.
+     * @param inverted True for inverted, false if not.
+     * @param master   The NomadSparkMAX to follow.
+     */
+    public NomadSparkMaxBrushed(int port, boolean inverted, NomadSparkMaxBrushed master) {
+        this(port, inverted);
+        follow(master);
+    }
+}
